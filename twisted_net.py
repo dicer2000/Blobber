8053771d431f0b22815a37de9dbbc0d3af1b558d
Brett Huffman <dicer2000@yahoo.com>
2023-11-29 09:05:42 -0600

    Added updated Twisted lib

--- twisted_net.py
+++ twisted_net.py
@@ -1,6 +1,7 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import protocol
 from settings import VERBOSITY
+import uuid
 
 # Define TCP Handlers
 # Twisted documentation found here: https://twisted.org/documents/9.0.0/core/howto/
@@ -9,14 +10,23 @@ class BlobberServerProtocol(protocol.Protocol):
     ''' Handles TCP connections for the server side of the game. '''
     def connectionMade(self):
         ''' Called when a client connects to the server. Logs the connection and adds the new player to the game. '''
+        self.clientID = str(uuid.uuid4())  # Generate a unique client ID
+        self.factory.addClient(self.clientID, self)
+
         if VERBOSITY > 2:
-            print(f"Connection from {self.transport.getPeer()}")
-        self.factory.game.add_player(self)
+            print(f"Connection from {self.transport.getPeer()} with ID {self.clientID}")
+
+    def connectionLost(self, reason):
+        ''' Called when the client connection is lost. Removes the client from the factory's list. '''
+        self.factory.removeClient(self.clientID)
 
     def dataReceived(self, data):
         ''' Called when data is received from a client. Logs the received data and handles player actions. '''
         if VERBOSITY > 2:
-            print(f"Received: {data}")
+            print(f"Received from {self.clientID}: {data}")
+        # The data is expected to be a pickled dictionary
+        # Send it to Main to process
+        self.factory.game.TCPDataReceivedFromClient(self.clientID, data)
 
 class BlobberServerFactory(protocol.Factory):
     ''' Factory for creating BlobberServerProtocol instances. '''
@@ -25,17 +35,45 @@ class BlobberServerFactory(protocol.Factory):
     def __init__(self, game):
         ''' Initializes the factory with a reference to the game instance. '''
         self.game = game
+        self.clients = {}  # Dictionary to track connected clients
+
+    def addClient(self, clientID, protocol):
+        ''' Adds a connected client to the factory's list. '''
+        self.clients[clientID] = protocol
+
+    def removeClient(self, clientID):
+        ''' Removes a client from the factory's list. '''
+        if clientID in self.clients:
+            del self.clients[clientID]
+
+    def sendMessageToClient(self, clientID, message):
+        ''' Sends a message to a specific client. '''
+        if clientID in self.clients:
+            self.clients[clientID].transport.write(message)
 
 class BlobberClientProtocol(protocol.Protocol):
+    def __init__(self, game):
+        self.game = game
+
     ''' Handles TCP connections for the client side of the game. '''
     def connectionMade(self):
         ''' Called when the client successfully connects to the server. Logs the connection and can send initial data to the server. '''
         print("Connected to the server.")
+        # Now have the initial data sent from the client to the server
+        # This is the only way to ensure the connection happens fully
+        # before sending the data
+        self.game.start_twisted_client_setup_user()
 
     def dataReceived(self, data):
         ''' Called when data is received from the server. Logs the received data and updates the game state based on this data. '''
         if VERBOSITY > 2:
             print(f"Data received from server: {data}")
+
+    def sendData(self, pickled_data):
+        # Send data to the server.  We always expect
+        # the data in pickled binary format
+        if self.transport:
+            self.transport.write(pickled_data)
 
 class BlobberClientFactory(protocol.ClientFactory):
     ''' Factory for creating BlobberClientProtocol instances. '''
@@ -44,7 +82,18 @@ class BlobberClientFactory(protocol.ClientFactory):
     def __init__(self, game):
         ''' Initializes the factory with a reference to the game instance. '''
         self.game = game
+        self.protocol_instance = None
 
+    def buildProtocol(self, addr):
+        self.protocol_instance = self.protocol(self.game)
+        return self.protocol_instance
+
+    def sendData(self, data):
+        if self.protocol_instance:
+            self.protocol_instance.sendData(data)
+        else:
+            print("No active protocol instance.")
+            
     def clientConnectionFailed(self, connector, reason):
         ''' Called when the client connection fails. Logs the reason for failure. '''
         print(f"Connection failed: {reason}")
